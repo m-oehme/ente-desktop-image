0fc34ddb4a23e674239107fbbaae4e7bd698eaf8
m-oehme <max.oehme@proton.me>
2025-07-20 00:25:53 +0200

    fix variablewq

--- build-container.sh
+++ build-container.sh
@@ -3,5 +3,5 @@
 echo "Version to pull $VERSION"
 
 wget https://github.com/ente-io/photos-desktop/releases/download/$VERSION/latest-linux.yml
-FILE_NAME=$(yq -r '.files[] | select(.url | contains(".deb")).url' latest-linux.yml)
+FILENAME=$(yq -r '.files[] | select(.url | contains(".deb")).url' latest-linux.yml)
 echo "filename=$FILENAME"
