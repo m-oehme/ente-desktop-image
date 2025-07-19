57d421151cc7960c9ecc2396eb043a06ee56b992
m-oehme <max.oehme@proton.me>
2025-07-20 00:36:26 +0200

    fix github output

--- build-container.sh
+++ build-container.sh
@@ -4,4 +4,4 @@ echo "Version to pull $VERSION"
 
 wget https://github.com/ente-io/photos-desktop/releases/download/$VERSION/latest-linux.yml
 FILENAME=$(yq -r '.files[] | select(.url | contains(".deb")).url' latest-linux.yml)
-echo "filename=$FILENAME"
+echo "filename=$FILENAME" >> $GITHUB_OUTPUT
